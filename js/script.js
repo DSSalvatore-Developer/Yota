ea2b2701729ce4580d7fab1387d8cfe40592df9e
DSSalvatore-Developer <D.S.Salvatore@iCloud.com>
2025-04-28 18:45:05 +0700

    script.js

--- js/script.js
+++ js/script.js
@@ -1,47 +1,14 @@
-// Моковые данные для авторизации
-const validCredentials = {
-  username: 'admin',
-  password: 'admin123'
-};
-
-// Обработчик формы авторизации
+// Обработчик для скрытия/показа текста в полях ввода
 document.getElementById('login-form').addEventListener('submit', function(event) {
   event.preventDefault();
   
   const username = document.getElementById('username').value;
   const password = document.getElementById('password').value;
-  
-  if (username === validCredentials.username && password === validCredentials.password) {
-    window.location.href = "reports/store1.html"; // Перенаправление на страницу отчетов
+
+  if (username && password) {
+    // Здесь можно добавить логику для авторизации
+    alert('Вы успешно вошли');
   } else {
-    alert('Неверный логин или пароль');
+    alert('Заполните все поля');
   }
-});
-
-// Отображение формы обратной связи
-function showContactForm() {
-  document.getElementById('contact-form').classList.remove('hidden');
-}
-
-// Отправка формы обратной связи
-document.querySelector('#contact-form-data').addEventListener('submit', function(event) {
-  event.preventDefault();
-
-  const email = document.getElementById('contact-email').value;
-  const subject = document.getElementById('contact-subject').value;
-  const message = document.getElementById('contact-message').value;
-
-  // Это имитирует отправку email
-  alert('Сообщение отправлено!');
-
-  // Очистка формы
-  document.getElementById('contact-form').classList.add('hidden');
-});
-
-// Обработчик для показа/скрытия пароля
-document.getElementById('toggle-password').addEventListener('click', function() {
-  const passwordField = document.getElementById('password');
-  const passwordType = passwordField.type === "password" ? "text" : "password";
-  passwordField.type = passwordType;
-  this.textContent = passwordType === "password" ? "👁️" : "🙈";
 });
