08ed701792772a0ff6fbacb0510425d47f9bbcb4
DSSalvatore-Developer <D.S.Salvatore@iCloud.com>
2025-04-28 16:46:52 +0700

    script.js

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ document.getElementById('login-form').addEventListener('submit', function(event)
   const password = document.getElementById('password').value;
   
   if (username === validCredentials.username && password === validCredentials.password) {
-    window.location.href = "reports.html"; // Перенаправление на страницу отчетов
+    window.location.href = "reports/store1.html"; // Перенаправление на страницу отчетов
   } else {
     alert('Неверный логин или пароль');
   }
@@ -24,10 +24,14 @@ function showContactForm() {
 }
 
 // Отправка формы обратной связи
-document.querySelector('#contact-form form').addEventListener('submit', function(event) {
+document.querySelector('#contact-form-data').addEventListener('submit', function(event) {
   event.preventDefault();
 
-  // Здесь будет код отправки письма через сервер
+  const email = document.getElementById('contact-email').value;
+  const subject = document.getElementById('contact-subject').value;
+  const message = document.getElementById('contact-message').value;
+
+  // Это имитирует отправку email
   alert('Сообщение отправлено!');
 
   // Очистка формы
